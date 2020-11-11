c7967aa503f7db36e57bfa761633882498baf7da
flail_monkey <flail-monkey@users.noreply.github.com>
2020-11-11 11:03:02 -0500

    Delete fwFunctions.ps1

--- fwFunctions.ps1
+++
@@ -1,9 +0,0 @@
-﻿# dump hashes from microsoft catalog file
-function Dump-Catalog($catPath, $sigcheckPath)
-    {
-    # provide path to catalog and sigcheck binary
-    # return array of hashes
-    $sigcheckPath = (gci $PSScriptRoot -Filter sigcheck.exe | Select-Object -ExpandProperty Fullname)
-    foreach ($hashString in ((& $sigcheckPath -d $catPath) | where-object {$_.Contains("Hash")}))
-        {$hashString.Split(":")[1].Trim()}
-    }
